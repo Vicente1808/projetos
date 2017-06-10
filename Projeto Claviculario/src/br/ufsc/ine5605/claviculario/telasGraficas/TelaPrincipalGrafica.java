8801658f31142f2ee395244ef7a139a693f0ae91
05169430906 <05169430906@UFSC164490.ufsc.br>
2017-06-09 21:57:07 -0300

    Atualizada tela principal

--- Projeto Claviculario/src/br/ufsc/ine5605/claviculario/telasGraficas/TelaPrincipalGrafica.java
+++ Projeto Claviculario/src/br/ufsc/ine5605/claviculario/telasGraficas/TelaPrincipalGrafica.java
@@ -8,11 +8,13 @@ package br.ufsc.ine5605.claviculario.telasGraficas;
 import br.ufsc.ine5605.claviculario.controladores.ControladorPrincipal;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
@@ -24,6 +26,7 @@ public class TelaPrincipalGrafica extends JFrame implements ActionListener {
     JButton btRetirar;
     JButton btDevolver;
     JButton btGerenciar;
+    JLabel lbTitulo;
     
     public TelaPrincipalGrafica(ControladorPrincipal owner) {
         super("Claviculário Eletrônico");
@@ -38,6 +41,7 @@ public class TelaPrincipalGrafica extends JFrame implements ActionListener {
         btRetirar = new JButton();
         btDevolver = new JButton();
         btGerenciar = new JButton();
+        lbTitulo = new JLabel();
         
         Insets insets = container.getInsets();
         
@@ -50,18 +54,25 @@ public class TelaPrincipalGrafica extends JFrame implements ActionListener {
         btGerenciar.setText("Gerenciar");
         btGerenciar.addActionListener(this);
         
+        lbTitulo.setText("Bem-Vindo ao Claviculário");
+        lbTitulo.setFont(new Font("Arial", Font.PLAIN, 20));
+        
         Dimension size = btRetirar.getPreferredSize();
-        btRetirar.setBounds(40 + insets.left, 50 + insets.top, 300 + size.width, 50 + size.height);
+        btRetirar.setBounds(40 + insets.left, 150 + insets.top, 300 + size.width, 50 + size.height);
         
         size = btDevolver.getPreferredSize();
-        btDevolver.setBounds(40 + insets.left, 150 + insets.top, 290 + size.width, 50 + size.height);
+        btDevolver.setBounds(40 + insets.left, 250 + insets.top, 290 + size.width, 50 + size.height);
         
         size = btGerenciar.getPreferredSize();
-        btGerenciar.setBounds(40 + insets.left, 250 + insets.top, 330 + size.width, 50 + size.height);
+        btGerenciar.setBounds(40 + insets.left, 350 + insets.top, 330 + size.width, 50 + size.height);
+        
+        size = lbTitulo.getPreferredSize();
+        lbTitulo.setBounds(180 + insets.left, 50 + insets.top, 200 + size.width, 25 + size.height);
         
         container.add(btRetirar);
         container.add(btDevolver);
         container.add(btGerenciar);
+        container.add(lbTitulo);
         
         setSize(500, 500);
         setLocationRelativeTo(null);
