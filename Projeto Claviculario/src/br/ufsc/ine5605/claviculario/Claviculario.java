369635abc8fc193b669ce34b47a196063f7d32e3
Adri <Adri@Adri-PC>
2017-06-18 18:19:12 -0300

    Adicionar veiculo

--- Projeto Claviculario/src/br/ufsc/ine5605/claviculario/Claviculario.java
+++ Projeto Claviculario/src/br/ufsc/ine5605/claviculario/Claviculario.java
@@ -8,6 +8,7 @@ import br.ufsc.ine5605.claviculario.entidades.Funcionario;
 import br.ufsc.ine5605.claviculario.entidades.Veiculo;
 import br.ufsc.ine5605.claviculario.persistencia.MapeadorFuncionario;
 import br.ufsc.ine5605.claviculario.valueObjects.FuncionarioVO;
+import br.ufsc.ine5605.claviculario.valueObjects.VeiculoVO;
 import java.util.Calendar;
 /**
  *
@@ -30,12 +31,17 @@ public class Claviculario {
         Funcionario funcionario2 = new Funcionario(596,"Pedro", Calendar.getInstance(), "48998288680","assistente");   
         Funcionario funcionario3 = new Funcionario(597,"Vinicius", Calendar.getInstance(), "48998288680","diretor");   
          
-        //Veiculo veiculo = new Veiculo("aaa-1234", "GTO", "Pontiac", 1969, 100000);
+        VeiculoVO veiculoVO = new VeiculoVO();
+        veiculoVO.placa = "AAA-1234";
+        veiculoVO.marca = "RAM";
+        veiculoVO.modelo = "dodge";
+        veiculoVO.ano = 1998;
+        veiculoVO.kmAtual = 1234;
+        ControladorVeiculos.getInstance().cadastrarVeiculo(veiculoVO);
         //Veiculo veiculo2 = new Veiculo("bbb-1234", "RAM", "DODGE", 2016, 1000);
         //Veiculo veiculo3 = new Veiculo("ccc-1234", "Gallardo", "Lamborguini", 2017, 0);
         //Veiculo veiculo4 = new Veiculo("ddd-1234", "MAVERICK", "FORD", 1969, 50000);
         
-        //ControladorVeiculos.getInstance().getVeiculos().add(veiculo);
         //ControladorVeiculos.getInstance().getVeiculos().add(veiculo2);
         //ControladorVeiculos.getInstance().getVeiculos().add(veiculo3);
         //ControladorVeiculos.getInstance().getVeiculos().add(veiculo4);
