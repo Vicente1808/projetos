24dff535c6737c4d65afc7f8bef2236dee56ec84
Vicente1808 <pablofigueirense@live.com>
2017-06-18 19:22:48 -0300

    mexi nos comanado

--- Projeto Claviculario/src/br/ufsc/ine5605/claviculario/telasGraficas/TelaDadosFuncionario.java
+++ Projeto Claviculario/src/br/ufsc/ine5605/claviculario/telasGraficas/TelaDadosFuncionario.java
@@ -19,7 +19,6 @@ import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
@@ -27,7 +26,6 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.ListModel;
 import javax.swing.text.DefaultFormatterFactory;
 import javax.swing.text.MaskFormatter;
 import javax.swing.text.NumberFormatter;
@@ -47,16 +45,16 @@ public class TelaDadosFuncionario extends JFrame implements ActionListener{
     private JLabel lbCargo;
     private JLabel lbBloqueado;
     private JLabel infoTela;
-    private JLabel veiculoPendente; 
+    private JLabel lbVeiculoPendente; 
         
-    private JFormattedTextField btMatricula;
-    private JTextField nome;
-    private JFormattedTextField dataNascimento;
-    private JFormattedTextField telefone;
-    private JComboBox cargo;
-    private JCheckBox bloqueado;
-    private JButton cadastrar;
-    private JTextField cargoOutro;
+    private JFormattedTextField tfMatricula;
+    private JTextField tfNome;
+    private JFormattedTextField tfDataNascimento;
+    private JFormattedTextField tfTelefone;
+    private JComboBox tfCargo;
+    private JComboBox tfBloqueado;
+    private JButton btCadastrar;
+    private JTextField tfCargoOutro;
     
     public TelaDadosFuncionario(ControladorFuncionarios controladorFuncionarios){
         this.controladorFuncionarios = controladorFuncionarios;
@@ -71,31 +69,31 @@ public class TelaDadosFuncionario extends JFrame implements ActionListener{
         lbTelefone = new JLabel();
         lbCargo = new JLabel();
         lbBloqueado = new JLabel();
-        veiculoPendente = new JLabel();
+        lbVeiculoPendente = new JLabel();
         infoTela = new JLabel();
         
         String[] cargos = {"Diretor","Outro"};
+        String[] status = {"Liberado","Bloqueado"};
         
-        
-        btMatricula = new JFormattedTextField(new DefaultFormatterFactory(new NumberFormatter(NumberFormat.getIntegerInstance())));
-        btMatricula.setText("");
-        nome = new JTextField();
+        tfMatricula = new JFormattedTextField(new DefaultFormatterFactory(new NumberFormatter(NumberFormat.getIntegerInstance())));
+        tfMatricula.setText("");
+        tfNome = new JTextField();
         
         try {
-            dataNascimento = new JFormattedTextField(new MaskFormatter("##/##/####"));
+            tfDataNascimento = new JFormattedTextField(new MaskFormatter("##/##/####"));
         } catch (ParseException ex) {
             Logger.getLogger(TelaDadosFuncionario.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         try {
-            telefone = new JFormattedTextField(new MaskFormatter("(##)#####-####"));
+            tfTelefone = new JFormattedTextField(new MaskFormatter("(##)#####-####"));
         } catch (ParseException ex) {
             Logger.getLogger(TelaDadosFuncionario.class.getName()).log(Level.SEVERE, null, ex);
         }
-        cargo = new JComboBox(cargos);
-        bloqueado = new JCheckBox();
-        cadastrar = new JButton();
-        cargoOutro = new JTextField();
+        tfCargo = new JComboBox(cargos);
+        tfBloqueado = new JComboBox(status);
+        btCadastrar = new JButton();
+        tfCargoOutro = new JTextField();
         
         Container container = getContentPane();
         container.setLayout(null);
@@ -109,49 +107,48 @@ public class TelaDadosFuncionario extends JFrame implements ActionListener{
         lbMatricula.setText("Matrícula:");
         lbMatricula.setBounds(50, 50, 100, 50);
         
-        btMatricula.setBounds(170, 60, 120, 25);
+        tfMatricula.setBounds(170, 60, 120, 25);
         
         lbNome.setText("Nome:");
         lbNome.setBounds(50, 100, 100, 50);
         
-        nome.setBounds(170, 110, 120, 25);
+        tfNome.setBounds(170, 110, 120, 25);
         
         lbDataNascimento.setText("Data Nascimento:");
         lbDataNascimento.setBounds(50, 150, 100, 50);
         
-        dataNascimento.setBounds(170, 160, 120, 25);
+        tfDataNascimento.setBounds(170, 160, 120, 25);
         
         lbTelefone.setText("Telefone:");
         lbTelefone.setBounds(50, 200, 100, 50);
         
-        telefone.setBounds(170, 210, 120, 25);
+        tfTelefone.setBounds(170, 210, 120, 25);
         
         lbCargo.setText("Cargo:");
         lbCargo.setBounds(50, 250, 100, 50);
         
-        cargo.setBounds(170, 260, 120, 25);
-        cargo.addActionListener(this);
-        cargo.setActionCommand("TROCACARGO");
-        cargoOutro.setBounds(170, 290, 120, 25);
+        tfCargo.setBounds(170, 260, 120, 25);
+        tfCargo.addActionListener(this);
+        tfCargo.setActionCommand("TROCACARGO");
+        tfCargoOutro.setBounds(170, 290, 120, 25);
         
         lbBloqueado.setText("Status:");      
         lbBloqueado.setBounds(50, 300, 100, 50);
         
-        bloqueado.setText("Bloqueado");
-        bloqueado.setBounds(170, 315, 120, 25);
+        tfBloqueado.setBounds(170, 315, 120, 25);
         
-        veiculoPendente.setText("Veiculo Pendente:         AAA-1234");
-        veiculoPendente.setBounds(50, 340, 240, 25);
-        veiculoPendente.setEnabled(false);
+        lbVeiculoPendente.setText("Veiculo Pendente:         AAA-1234");
+        lbVeiculoPendente.setBounds(50, 340, 240, 25);
+        lbVeiculoPendente.setEnabled(false);
         
-        cadastrar.setText("Cadastrar");
-        cadastrar.setBounds(50, 370, 120, 30);
-        cadastrar.addActionListener(this);
+        btCadastrar.setText("Cadastrar");
+        btCadastrar.setBounds(50, 370, 120, 30);
+        btCadastrar.addActionListener(this);
         
         infoTela.setBounds(320, 370, 330, 25);
         infoTela.setForeground(Color.red);
         
-        String[][] placas = ControladorFuncionarios.getInstance().getVeiculosSimples();
+        //String[][] placas = ControladorFuncionarios.getInstance().getVeiculosSimples();
         JList painel = new JList();
         
         //painel.setLineWrap(rootPaneCheckingEnabled);
@@ -168,76 +165,76 @@ public class TelaDadosFuncionario extends JFrame implements ActionListener{
         container.add(lbTelefone);
         container.add(lbCargo);
         container.add(lbBloqueado);
-        container.add(veiculoPendente);
+        container.add(lbVeiculoPendente);
         
-        container.add(btMatricula);
-        container.add(nome);
-        container.add(dataNascimento);
-        container.add(telefone);
-        container.add(cargo);
-        container.add(bloqueado);
-        container.add(cadastrar);
+        container.add(tfMatricula);
+        container.add(tfNome);
+        container.add(tfDataNascimento);
+        container.add(tfTelefone);
+        container.add(tfCargo);
+        container.add(tfBloqueado);
+        container.add(btCadastrar);
         container.add(jscroll);
         container.add(infoTela);
         
         setSize(680,460);
         setLocationRelativeTo(null);
         
-        if(cargo.getSelectedItem().toString().equals("Outro")){
-                container.add(cargoOutro);
+        if(tfCargo.getSelectedItem().toString().equals("Outro")){
+                container.add(tfCargoOutro);
             }
     }   
             
+    public void limparTela(){
+        tfMatricula.setText("");
+        tfNome.setText("");
+        tfDataNascimento.setText("");        
+        tfTelefone.setText("");        
+        tfBloqueado.validate();
+        lbVeiculoPendente.setText("");        
+    }
     
     @Override
     public void actionPerformed(ActionEvent ae) {
         
         if(ae.getActionCommand().equals("TROCACARGO")){
-            if(cargo.getSelectedItem().toString().equals("Outro")){                
-                getContentPane().add(cargoOutro);
+            if(tfCargo.getSelectedItem().toString().equals("Outro")){                
+                getContentPane().add(tfCargoOutro);
             }else{
-                getContentPane().remove(cargoOutro);
+                getContentPane().remove(tfCargoOutro);
             }
         }
         
-        if(ae.getSource() == cadastrar) {
-            if(btMatricula.getText().equals("")){
+        if(ae.getSource() == btCadastrar) {
+            if(tfMatricula.getText().equals("")){
                 infoTela.setText("Campo Matrícula está em branco.");
+                infoTela.setForeground(Color.BLUE);
                 repaint();
-            }else if(nome.getText().equals("")){
+            }else if(tfNome.getText().equals("")){
                 infoTela.setText("Campo nome está em branco.");
+                infoTela.setForeground(Color.green);
                 repaint();               
-            }else if(dataNascimento.getText().equals("  /  /    ")){
+            }else if(tfDataNascimento.getText().equals("  /  /    ")){
                 infoTela.setText("Campo data nascimento está em branco.");
+                infoTela.setForeground(Color.ORANGE);
                 repaint();     
-            }else if(telefone.getText().equals("(  )     -    ")){
+            }else if(tfTelefone.getText().equals("(  )     -    ")){
                 infoTela.setText("Campo telefone está em branco.");
+                infoTela.setForeground(Color.MAGENTA);
                 repaint();                 
-            }else if(cargo.getSelectedItem().equals("Outro")&&cargoOutro.getText().equals("")){
+            }else if(tfCargo.getSelectedItem().equals("Outro")&&tfCargoOutro.getText().equals("")){
                 infoTela.setText("Campo cargo está em branco.");
+                infoTela.setForeground(Color.MAGENTA);
                 repaint(); 
             }else{
-                /*
-                int novaMatricula =0;
-                String mat = null;
-                
-                String[] dados = btMatricula.getText().split(".");
-                if(dados.length==4){
-                    for(int i=0; i < dados.length;i++){
-                        if(!(dados[i].equals(""))){
-                            mat = mat + dados[i];
-                        }
-                    }
-                    novaMatricula = Integer.parseInt(mat);        
-                }
-                */
+
                 FuncionarioVO funcionarioVO = new FuncionarioVO();
-                funcionarioVO.matricula = Integer.parseInt(btMatricula.getText());
-                funcionarioVO.nome = nome.getText();
+                funcionarioVO.matricula = Integer.parseInt(tfMatricula.getText());
+                funcionarioVO.nome = tfNome.getText();
                 
                 SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
  
-                String data = dataNascimento.getText();
+                String data = tfDataNascimento.getText();
                 Calendar c = Calendar.getInstance();
                 try {
                     c.setTime(formatoData.parse(data));
@@ -247,19 +244,24 @@ public class TelaDadosFuncionario extends JFrame implements ActionListener{
                     repaint();                     
                 }
                 
-                funcionarioVO.telefone = telefone.getText();
+                funcionarioVO.telefone = tfTelefone.getText();
                 
-                if(cargo.getSelectedItem().equals("Outro")&&cargoOutro.getText().equals("")){
-                    funcionarioVO.cargo = cargoOutro.getText();
+                if(tfCargo.getSelectedItem().equals("Outro")&&tfCargoOutro.getText().equals("")){
+                    funcionarioVO.cargo = tfCargoOutro.getText();
                 }else{
-                    funcionarioVO.cargo = cargo.getSelectedItem().toString();
+                    funcionarioVO.cargo = tfCargo.getSelectedItem().toString();
                 }
                 
-                funcionarioVO.bloqueado = bloqueado.isBorderPaintedFlat();
+                if(tfBloqueado.getSelectedItem().equals("Disponível")){
+                    funcionarioVO.bloqueado =false;
+                }else{
+                        funcionarioVO.bloqueado =true;
+                        }            
+                //funcionarioVO.bloqueado = tfBloqueado.isBorderPaintedFlat();
                 funcionarioVO.veiculoPendente = null;
                 
-                infoTela.setText("Processando cadastro....");
-                
+                limparTela();
+                infoTela.setForeground(Color.blue);
                 infoTela.setText(controladorFuncionarios.cadastrarFuncionario(funcionarioVO));
                 
                 
